ea9828f5117f07955808193a027106ed2b7f15f5
Tomas "Jethro" Pokorny <xtompok@gmail.com>
2016-12-28 18:37:03 +0100

    Changed PBF structure, trips sorted by time.

--- raptor/prepare.py
+++ raptor/prepare.py
@@ -19,17 +19,18 @@ except:
 	sys.exit(1)
 cur = conn.cursor(cursor_factory = psycopg2.extras.DictCursor)
 
+pbtt = data_pb2.Timetable()
 
 #Stops
 cur.execute("SELECT * FROM gtfs_stops")
 stops = cur.fetchall()
 stoplut = {}
 stopilut = {}
-pbstops = data_pb2.Stops()
+pbstops = pbtt.stops
 for (newid,stop) in enumerate (stops):
 	stoplut[stop["stop_id"]]=newid
 	stopilut[newid]=stop["stop_id"]
-	pbstop = pbstops.stops.add()
+	pbstop = pbstops.add()
 	pbstop.name = stop["stop_name"] 
 	pbstop.id = newid
 	
@@ -40,7 +41,7 @@ class Subroute(object):
 	maxid = 0
 	stop_seqs = {}
 
-	def __init__(self,stimes):
+	def __init__(self,stimes,name=None):
 		stops = tuple(map(lambda x:stoplut[x["stop_id"]], stimes))
 		pbtimes = []
 		for stop in stimes:
@@ -53,24 +54,24 @@ class Subroute(object):
 			self.stop_seqs[stops].append(pbtimes)
 			self.valid = False
 		else:
-			self.new(pbtimes,stops)
+			self.new(pbtimes,stops,name)
 			self.stop_seqs[stops] = self
 			self.valid = True
 
 		
-	def new(self,pbtimes,stops):
+	def new(self,pbtimes,stops,name):
 		self.pbroute = data_pb2.Route()
 		self.pbroute.id = Subroute.maxid
 		self.pbroute.ntrips = 1
 		self.pbroute.nstops = len(stops)
+		if name:
+			self.pbroute.name = name
 
 		Subroute.maxid += 1
 
 		self.stops = stops
 		self.triptimes = [pbtimes]
 
-		
-	
 	def append(self,pbtimes):
 		self.triptimes.append(pbtimes)
 		self.pbroute.ntrips += 1
@@ -79,68 +80,60 @@ class Subroute(object):
 		self.pbroute.stopsidx = len(rstops)
 		self.pbroute.tripsidx = len(trips)
 		rstops.extend(self.stops)
+		self.triptimes.sort(key=lambda x:x[0].departure)
 		for triptime in self.triptimes:
 			trips.extend(triptime)
 		routes.extend([self.pbroute])
 
 	
 		
-cur.execute("SELECT * FROM gtfs_routes;")
+cur.execute("SELECT * FROM gtfs_routes ORDER BY route_short_name;")
 routes = cur.fetchall()
 
 subroutes = []
 
-for route in routes[:6]:
+for route in routes:
 	print(route["route_short_name"])
-	cur.execute("SELECT * FROM gtfs_trips WHERE route_id = %s;",(route["route_id"],))
+	cur.execute("SELECT * FROM gtfs_trips WHERE route_id = %s ORDER BY trip_id;",(route["route_id"],))
 	trips = cur.fetchall()
 
 	for trip in trips:
 		cur.execute("SELECT * FROM gtfs_stop_times WHERE trip_id = %s ORDER BY stop_sequence",(trip["trip_id"],))
 		stimes = cur.fetchall()
-		sr = Subroute(stimes)
+		sr = Subroute(stimes,route["route_short_name"])
 		if sr.valid:
 			subroutes.append(sr)
 
 
-pbtimes = data_pb2.StopTimes()
-pbroutes = data_pb2.Routes()
-pbrstops = data_pb2.RouteStops()
+pbtimes = pbtt.stop_times
+pbroutes = pbtt.routes
+pbrstops = pbtt.route_stops
 
 for sr in subroutes:
-	sr.flush(pbrstops.stop_ids,pbtimes.stop_times,pbroutes.routes)
+	sr.flush(pbrstops,pbtimes,pbroutes)
 
-pbsroutes = data_pb2.StopRoutes()
-pbtransfers = data_pb2.Transfers()
+pbsroutes = pbtt.stop_routes
+pbtransfers = pbtt.transfers
 sroutes = {}
 
-for route in pbroutes.routes:
-	for stop in pbrstops.stop_ids[route.stopsidx:route.stopsidx+route.nstops]:
+for route in pbroutes:
+	for stop in pbrstops[route.stopsidx:route.stopsidx+route.nstops]:
 		if stop in sroutes:
 			sroutes[stop].append(route.id)
 		else:
 			sroutes[stop] = [route.id]
 
 routecnt = 0
-for stop in pbstops.stops:
+for stop in pbstops:
 	routes = sroutes.get(stop.id,[])
 	stop.nroutes=len(routes)
 	stop.routeidx=routecnt
-	pbsroutes.route_ids.extend(routes)
+	pbsroutes.extend(routes)
 	routecnt += len(routes)
 
 	stop.ntransfers = 0
 	stop.transferidx = 0
 
-pbtt = data_pb2.Timetable()
-pbtt.routes.CopyFrom(pbroutes)
-pbtt.stop_times.CopyFrom(pbtimes)
-pbtt.route_stops.CopyFrom(pbrstops)
-pbtt.stops.CopyFrom(pbstops)
-pbtt.transfers.CopyFrom(data_pb2.Transfers())
-pbtt.stop_routes.CopyFrom(pbsroutes)
 
 with open("tt.bin","wb") as ttfile:
 	ttfile.write(pbtt.SerializeToString())
-print(pbroutes)
-print(pbstops)
